5096de151049bd24db5208d044aee7dd83b61681
Xintong Jiang <231744781+xintong-J@users.noreply.github.com>
2025-09-18 14:12:31 -0400

    You are scared right now.

--- story.py
+++ story.py
@@ -11,6 +11,7 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("You chose left side and met a big bat!")
+    print("You are scared right now.")
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
 
